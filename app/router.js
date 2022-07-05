f69c067eb5caecaff5515571089d7da406d42aae
wanxing-project <374797861@qq.com>
2022-07-05 16:54:05 +0800

    feat: 修改路由

--- app/router.js
+++ app/router.js
@@ -6,5 +6,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/demo1', controller.demo1.index);
+  router.get('/demo1/getList', controller.demo1.index);
 };
