ca896f5ba5e78a8cd0e87dea3f736c64128826c5
wanxing-project <374797861@qq.com>
2022-07-07 21:19:07 +0800

    feat: 无条件查询多条语句

--- app/controller/demo1.js
+++ app/controller/demo1.js
@@ -5,10 +5,15 @@ const Controller = require('egg').Controller;
 class Demo1Controller extends Controller {
   async getList() {
     const { ctx } = this;
-
-    // ctx.body = 'hi, test111111111111111111111111111111';
-
-    let users = await ctx.service.demo1.getUserById();
+    const data = ctx.query; // condition
+    let param = { // 搜索 post 表
+      // where: { title: 'Hello World', age: [10, 11] }, // WHERE 条件
+      // columns: ['title'], // 要查询的表字段
+      // orders: [['id', 'desc']], // 排序方式 desc降序 
+      // limit: 10, // 返回数据量
+      // offset: 0, // 数据偏移量
+    }
+    let users = await ctx.service.demo1.getList(param);
     ctx.body = users;
   }
 }
--- app/service/demo1.js
+++ app/service/demo1.js
@@ -3,10 +3,10 @@
 const Service = require('egg').Service;
 
 class Demo1Service extends Service {
-    async getUserById(id) {
-        // 根据id查询用户信息
-        // if（）
-        return await this.app.mysql.get('dormitory_info', {});
+    async getList(param) {
+        // this.app.mysql.get('dormitory_info', {}); // 不论{}内是什么参数，get永远只有一条数据
+        // this.app.mysql.select('dormitory_info', {}); // select是查询多条语句
+        return await this.app.mysql.select('dormitory_info', param);
     }
 }
 module.exports = Demo1Service;
